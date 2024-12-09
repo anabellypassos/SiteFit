9019fbeb6444c53b7acf1005724e95388f2b5725
Anabelly Passos <anabellybel123@gmail.com>
2024-12-09 10:49:39 -0300

    quadrante6

--- src/Pages/quadrante06/quadrante06.js
+++ src/Pages/quadrante06/quadrante06.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import '../quadrante06/quadrante06.css';
 
-const   Quadrante06 = () => {
+const Quadrante06 = () => {
   return (
     <div className='quadrante06'>
-      
+      <h1 className='title6'>Depoimentos</h1>
+
+      <div className='quadrantes6'>
+        < div className='quadrantecontainer1' >
+          <p className='depoimento1'>
+            Depoimentos são frases <br />
+            curtas das pessoas que <br />
+            gostam da sua marca. É uma <br />
+            ótima forma de convencer o<br />
+            público a experimentar seus <br />
+             serviços.
+
+            <p className='pessoa1'>
+              Francine, Assistente <br />
+              Virtual
+            </p>
+          </p>
+        </div>
+
+        < div className='quadrantecontainer2' >
+          <p className='depoimento2'>
+            Depoimentos são frases <br />
+            curtas das pessoas que <br />
+            gostam da sua marca. É uma <br />
+            ótima forma de convencer o<br />
+            público a experimentarseus<br /> 
+             serviços.
+
+            <p className='pessoa2'>
+              Francine, Assistente <br />
+              Virtual
+            </p>
+          </p>
+
+        </div>
+        < div className='quadrantecontainer3'>
+          <p className='depoimento3'>
+            Depoimentos são frases <br />
+            curtas das pessoas que <br />
+            gostam da sua marca. É uma <br />
+            ótima forma de convencer o<br />
+            público a experimentar seus<br />
+            serviços.
+
+            <p className='pessoa3'>
+              Francine, Assistente <br />
+              Virtual
+            </p>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default  Quadrante06;
+export default Quadrante06;
