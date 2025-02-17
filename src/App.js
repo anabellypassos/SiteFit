71544d36678dcb937e897db7bd12d1e14e1604dc
Anabelly Passos <anabellybel123@gmail.com>
2025-02-17 17:05:55 -0300

    quadrante4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AppBar from './Pages/AppBar/appBar';
 import Quadrante01 from './Pages/quadrante01/quadrante01';
 import  Quadrante02 from './Pages/quadrante02/quadrante02'; 
 import Quadrande03 from  './Pages/quadrante3/quadrante3';
+import  Quadrante04 from './Pages/quadrante04/quadrante04';
 function App() {
   return (
     <div className='quadrantesPages'>
@@ -10,6 +11,7 @@ function App() {
       <Quadrante01/>
       <Quadrante02/>
       <Quadrande03/>
+      <Quadrante04/>
     </div>
   );
 }
--- src/Pages/quadrante04/quadrante04.js
+++ src/Pages/quadrante04/quadrante04.js
@@ -9,9 +9,9 @@ const Quadrante04 = () => {
             Treinos e <br/>
             exercícios
             </h1>
-            <p className='paregrafos'>Fale sobre as características mais importantes deste produto <br/>
-                 ou serviço. Destacar as mais populares funciona também! Fale sobre<br/>
-                  as características mais importantes deste produto ou serviço. <br/>
+            <p className='paregrafos'>Fale sobre as características mais importantes deste produto 
+                 ou serviço. Destacar as mais populares funciona também! Fale sobre
+                  as características mais importantes deste produto ou serviço. 
                   Destacar as mais populares funciona também!</p>
  <button className='saibamais'>SAIBA MAIS</button>
 
