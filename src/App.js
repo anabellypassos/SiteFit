b7839fe8b9ea0a0019d263b048d101f6bdc603d2
Anabelly Passos <anabellybel123@gmail.com>
2025-02-21 11:05:50 -0300

    quadrante5

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Quadrante01 from './Pages/quadrante01/quadrante01';
 import  Quadrante02 from './Pages/quadrante02/quadrante02'; 
 import Quadrande03 from  './Pages/quadrante3/quadrante3';
 import  Quadrante04 from './Pages/quadrante04/quadrante04';
+import Quadrante05 from './Pages/quadrante05/quadrante05';
 function App() {
   return (
     <div className='quadrantesPages'>
@@ -12,6 +13,7 @@ function App() {
       <Quadrante02/>
       <Quadrande03/>
       <Quadrante04/>
+      <Quadrante05/>
     </div>
   );
 }
--- src/Pages/quadrante05/quadrante05.js
+++ src/Pages/quadrante05/quadrante05.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import '../quadrante05/quadrante05.css';
-import mulherbebendoagua from '../quadrante05/image/mulherhjd.png';
-import fundocolorido from '../../images/Colorful-Holographic-Gradient-Multicolor-Blurred-Background-Design-63936110-1.png';
 const Quadrante05 = () => {
   return (
     <div className='quadrante05'>
      
      <div className='sobreposicao'>
    
-     <img  className='mulherbebendoagua'src={mulherbebendoagua} alt='mulherbebendoagua'/>
+ 
      
 
     <div className='container5'>
@@ -17,11 +15,11 @@ const Quadrante05 = () => {
         bem-estar
         </h1>
         <p className='dicaparagrafo'>
-        Fale sobre as características mais <br/> 
-        importantes deste produto ou serviço. <br/>
-        Destacar as mais populares funciona<br/>
-         também! Fale sobre as características<br/>
-        mais importantes deste produto<br/>
+        Fale sobre as características mais 
+        importantes deste produto ou serviço. 
+        Destacar as mais populares funciona
+         também! Fale sobre as características
+        mais importantes deste produto
         ou serviço.
         </p>
         <button className='btnsaibamais'>SAIBA MAIS</button>
